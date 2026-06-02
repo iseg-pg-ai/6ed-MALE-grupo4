e19f00065aae6ceaca7549a8ce0aa3f82b1d8111
Vitor Antunes <vitor.e.antunes@cstaf.pt>
2026-06-02 21:03:46 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("hello world")
+
+
+# import stuff
+
+# load train dataset
+
+# clean up dataset
+
+# do supervised learning
+
+# do unsupervised learning
+
+# load test dataset
+
+# check against created models
